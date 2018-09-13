310264a768b0660058ea0e6b017a6ef4b3d94c0d
Zac Willmington <zac.willmington@gmail.com>
2018-09-13 15:37:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ class Driver {
 
     passengers() {
         const passengersIds = this.trips().map(trip => trip.passengerId);
-        console.log(passengersIds, store.passengers);
         for (i = 0; i > store.passengers.length; i++){
             console.log(passenger[i]);
             // for(j = ; )
