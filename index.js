35c8ef8605dc5bd50e77b59968e95e0c4bf776ef
Zac Willmington <zac.willmington@gmail.com>
2018-09-13 14:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let store = { drivers: [], passengers: [] }
 
 
+let driverId = 0;
 
 class Driver {
     constructor(name){
         this.name = name;
+        this.id = ++driverId;
     }
+
+    store.drivers.push(this);
 }
 
 
