2620105460d64dc77deb5b0012dfa25bc96e543f
Zac Willmington <zac.willmington@gmail.com>
2018-09-13 14:45:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+class Driver {
+    constructor(name){
+        this.name = name;
+    }
+}
+
+
+
+
+
+class Passenger {
+    constructor(name){
+        this.name = name;
+    }
+}
+
+
+
+
+class Trip {
+
+}
